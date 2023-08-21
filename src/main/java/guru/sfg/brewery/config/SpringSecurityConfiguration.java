cfddbb6ecefbdd24cda259dcc7ae54ff631987a5
cvetkovicUros <140717978+cvetkovicUros@users.noreply.github.com>
2023-08-21 09:32:57 +0200

    Added userDetailsService (#6)
    added two users with roles admin,user in userDetailsService method -> SpringSecurityConfiguration.java
    
    Co-authored-by: Uros Cvetkovic <uros.cvetkovic@badin.rs>

--- src/main/java/guru/sfg/brewery/config/SpringSecurityConfiguration.java
+++ src/main/java/guru/sfg/brewery/config/SpringSecurityConfiguration.java
@@ -1,14 +1,19 @@
 package guru.sfg.brewery.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
-public class SpringConfiguration extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
     @Override
@@ -26,5 +31,23 @@ public class SpringConfiguration extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("uros")
+                .password("urospass")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin,user);
     }
 }
--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerTestIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerTestIT.java
@@ -3,6 +3,8 @@ package guru.sfg.brewery.web.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -20,6 +22,30 @@ public class BeerControllerTestIT extends BaseTestClass{
     @Test
     void findBeersHttpBasicAuth() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/beers/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    @Test
+    void initCreationFor() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/beers/new").with(httpBasic("user","password")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    @Test
+    void findBeersAuth() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/beers/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    @Test
+    void findBeersWithAnonymous() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/beers/find").with(anonymous()))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
                 .andExpect(model().attributeExists("beer"));
